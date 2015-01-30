993f51fb1d163c4eac87627aa18c721d2f979579
William Tang <wtang@squorange.com>
2015-01-30 00:06:13 -0800

    Update parts: funding, speaking

--- wp-content/themes/ag-theme/parts/speaking.php
+++ wp-content/themes/ag-theme/parts/speaking.php
@@ -3,8 +3,13 @@
 	<div class="row row--header">
     <div class="large-10 large-offset-1 columns">
 			<h1>
-				Please let us know if you'd like us to speak at your next event.
+			Please let us know if you'd like us to speak at your next event.
 			</h1>
+		</div>
+	</div>
+	<div class="row header__text-blurb">
+		<div class="large-8 large-offset-2 columns">
+			We are experienced and sought-after speakers and facilitators on topics including: Human leadership, Design Thinking, Experience Design (XD), creativity, innovation, performance management, values, creating beautiful organizations, people practices, and culture.
 		</div>
 	</div>
 	<div class="row row--last">
