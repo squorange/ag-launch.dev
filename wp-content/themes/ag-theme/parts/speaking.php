3ac46a2444d673400b6ef53a2f04308fb80091bc
William Tang <wtang@squorange.com>
2015-01-29 20:54:50 -0800

    Speak page, posts

---
+++ wp-content/themes/ag-theme/parts/speaking.php
@@ -0,0 +1,16 @@
+<section> <!-- BEGIN SPEAKING -->
+	
+	<div class="row row--header">
+    <div class="large-10 large-offset-1 columns">
+			<h1>
+				Please let us know if you'd like us to speak at your next event.
+			</h1>
+		</div>
+	</div>
+	<div class="row row--last">
+		<div class="large-4 large-offset-4 medium-6 medium-offset-3 columns">
+			<a href="mailto:contact@awakengroup.com" class="button expand">contact@awakengroup.com</a>
+		</div>
+	</div>	
+
+</section> <!-- END CAREERS -->
