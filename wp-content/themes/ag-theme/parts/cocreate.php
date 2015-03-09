3442e96d1105800152feec3ca6549ae8695310c7
William Tang <wtang@squorange.com>
2015-03-09 16:56:48 +0800

    Yellow Book button in Co-Create module

--- wp-content/themes/ag-theme/parts/cocreate.php
+++ wp-content/themes/ag-theme/parts/cocreate.php
@@ -3,13 +3,16 @@
 	<div class="row row--header">
     <div class="large-10 large-offset-1 columns">
 			<h1>
-			Please contact us to see how we can help you achieve your goals while living out your purpose and values
+			See how we can help you achieve your goals while living out your purpose and values
 			</h1>
 		</div>
 	</div>
 	<div class="row">
-		<div class="large-4 large-offset-4 medium-6 medium-offset-3 columns">
-			<a href="<?php echo home_url(); ?>/contact/" class="button expand">Co-Create with us</a>
+		<div class="large-4 large-offset-2 medium-6 columns">
+			<a href="<?php echo home_url(); ?>/yellowbook/" target="_blank" class="button expand">Read our Yellow Book</a>
+		</div>
+		<div class="large-4 large-offset-0 medium-6 columns end">
+			<a href="<?php echo home_url(); ?>/contact/" class="button expand">Contact us</a>
 		</div>
 	</div>	
 
