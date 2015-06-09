b48e14da39335611cf376b93276cd495657c9388
William Tang <wtang@squorange.com>
2015-06-09 10:26:51 +0800

    Update US address on contact

--- wp-content/themes/ag-theme/page-contact.php
+++ wp-content/themes/ag-theme/page-contact.php
@@ -38,8 +38,8 @@ Template Name: Contact
 				<div class="clearfix">
 				<address class="vcard"> 
 				<h2 class="text--bold org">USA</h2>
-				<div class="adr"><span class="street-address">445 S Figueroa #3100</span><br />
-				<span class="locality">Los Angeles</span> <abbr class="region" title="California">CA</abbr> <span class="postal-code">90071</span></div>
+				<div class="adr"><span class="street-address">5875 Green Valley Circle #200</span><br />
+				<span class="locality">Culver City</span> <abbr class="region" title="California">CA</abbr> <span class="postal-code">90230</span></div>
 				<div class="tel">+1 310 265 9029</div>
 				</address>
 				</div>
